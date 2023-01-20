60de41163ad4eca5c43e2492aeadc517ade50cfd
baejed <tristansubong@gmail.com>
2023-01-20 11:55:50 +0800

    gagsti

--- src/FirstYear2ndSem/AutoPornHubOpener.java
+++ src/FirstYear2ndSem/AutoPornHubOpener.java
@@ -9,6 +9,8 @@ public class AutoPornHubOpener {
 
     public static void main(String[] args) throws AWTException, InterruptedException {
 
+        String searchBarInput = "PORNHUB.COM";
+
         Robot robot = new Robot();
 
         robot.keyPress(KeyEvent.VK_WINDOWS);
@@ -30,30 +32,37 @@ public class AutoPornHubOpener {
         robot.keyRelease(16);
         robot.keyRelease(KeyEvent.VK_N);
 
-        robot.keyPress(KeyEvent.VK_P);
-        robot.keyRelease(KeyEvent.VK_P);
-        robot.keyPress(KeyEvent.VK_O);
-        robot.keyRelease(KeyEvent.VK_O);
-        robot.keyPress(KeyEvent.VK_R);
-        robot.keyRelease(KeyEvent.VK_R);
-        robot.keyPress(KeyEvent.VK_N);
-        robot.keyRelease(KeyEvent.VK_N);
-        robot.keyPress(KeyEvent.VK_H);
-        robot.keyRelease(KeyEvent.VK_H);
-        robot.keyPress(KeyEvent.VK_U);
-        robot.keyRelease(KeyEvent.VK_U);
-        robot.keyPress(KeyEvent.VK_B);
-        robot.keyRelease(KeyEvent.VK_B);
-        robot.keyPress(KeyEvent.VK_PERIOD);
-        robot.keyRelease(KeyEvent.VK_PERIOD);
-        robot.keyPress(KeyEvent.VK_C);
-        robot.keyRelease(KeyEvent.VK_C);
-        robot.keyPress(KeyEvent.VK_O);
-        robot.keyRelease(KeyEvent.VK_O);
-        robot.keyPress(KeyEvent.VK_M);
-        robot.keyRelease(KeyEvent.VK_M);
+        for(int i = 0; i < searchBarInput.length(); i++){
+            robot.keyPress(searchBarInput.charAt(i));
+            robot.keyRelease(searchBarInput.charAt(i));
+        }
+
         robot.keyPress(KeyEvent.VK_ENTER);
-        robot.keyRelease(KeyEvent.VK_ENTER);
+
+//        robot.keyPress(KeyEvent.VK_P);
+//        robot.keyRelease(KeyEvent.VK_P);
+//        robot.keyPress(KeyEvent.VK_O);
+//        robot.keyRelease(KeyEvent.VK_O);
+//        robot.keyPress(KeyEvent.VK_R);
+//        robot.keyRelease(KeyEvent.VK_R);
+//        robot.keyPress(KeyEvent.VK_N);
+//        robot.keyRelease(KeyEvent.VK_N);
+//        robot.keyPress(KeyEvent.VK_H);
+//        robot.keyRelease(KeyEvent.VK_H);
+//        robot.keyPress(KeyEvent.VK_U);
+//        robot.keyRelease(KeyEvent.VK_U);
+//        robot.keyPress(KeyEvent.VK_B);
+//        robot.keyRelease(KeyEvent.VK_B);
+//        robot.keyPress(KeyEvent.VK_PERIOD);
+//        robot.keyRelease(KeyEvent.VK_PERIOD);
+//        robot.keyPress(KeyEvent.VK_C);
+//        robot.keyRelease(KeyEvent.VK_C);
+//        robot.keyPress(KeyEvent.VK_O);
+//        robot.keyRelease(KeyEvent.VK_O);
+//        robot.keyPress(KeyEvent.VK_M);
+//        robot.keyRelease(KeyEvent.VK_M);
+//        robot.keyPress(KeyEvent.VK_ENTER);
+//        robot.keyRelease(KeyEvent.VK_ENTER);
 
 
 
